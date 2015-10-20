c11fcc7d1c61b9888c76d159c8f9b34959305154
Frank Pepermans <frank@igindo.com>
2015-10-20 23:11:22 +0100

    no message

--- test/all_tests.dart
+++ test/all_tests.dart
@@ -4,7 +4,7 @@ library buffer_when_test;
 
 import 'dart:async';
 import "package:test/test.dart" as test;
-import 'package:guinness/guinness_html.dart';
+import 'package:guinness/guinness_html.dart' as guinness;
 import 'package:rxdart/rxdart.dart' as Rx;
 
 Future testObservable(Rx.Observable observable, {expectation(List values)}) {
@@ -26,60 +26,60 @@ Future testObservable(Rx.Observable observable, {expectation(List values)}) {
 
 void main() {
   test.test('run all', () {
-    guinnessEnableHtmlMatchers();
+    guinness.guinnessEnableHtmlMatchers();
     
     Rx.Observable observable;
     
-    beforeEach(() {
+    guinness.beforeEach(() {
       observable = new Rx.Observable.from([1, 2, 3]);
     });
     
-    describe("Rx.Observable.from", () {
-      it("creates an Observable from a List", () {
+    guinness.describe("Rx.Observable.from", () {
+      guinness.it("creates an Observable from a List", () {
         return testObservable(new Rx.Observable.from([1, 2, 3]),
-          expectation: (values) => expect(values).toEqual([1, 2, 3]));
+          expectation: (values) => guinness.expect(values).toEqual([1, 2, 3]));
       });
     });
     
-    describe("Rx.Observable.range", () {
-      it("creates an Observable from a range", () {
+    guinness.describe("Rx.Observable.range", () {
+      guinness.it("creates an Observable from a range", () {
         return testObservable(new Rx.Observable.range(0, 3),
-          expectation: (values) => expect(values).toEqual([0, 1, 2]));
+          expectation: (values) => guinness.expect(values).toEqual([0, 1, 2]));
       });
     });
     
-    describe("Rx.Observable.fromFuture", () {
-      it("creates an Observable from a Future", () {
+    guinness.describe("Rx.Observable.fromFuture", () {
+      guinness.it("creates an Observable from a Future", () {
         return testObservable(new Rx.Observable.fromFuture(new Future<int>.value(0)),
-          expectation: (values) => expect(values).toEqual([0]));
+          expectation: (values) => guinness.expect(values).toEqual([0]));
       });
     });
     
-    describe("Rx.Observable.bufferWithCount", () {
-      it("buffers values and creates Lists to contain the values", () {
+    guinness.describe("Rx.Observable.bufferWithCount", () {
+      guinness.it("buffers values and creates Lists to contain the values", () {
         return testObservable(observable.bufferWithCount(2),
-          expectation: (values) => expect(values).toEqual([[1, 2], [3]]));
+          expectation: (values) => guinness.expect(values).toEqual([[1, 2], [3]]));
       });
     });
     
-    describe("Rx.Observable.debounce", () {
-      it("debounces an Observable using Duration", () {
+    guinness.describe("Rx.Observable.debounce", () {
+      guinness.it("debounces an Observable using Duration", () {
         return testObservable(observable.debounce(const Duration(seconds: 1)),
-          expectation: (values) => expect(values).toEqual([3]));
+          expectation: (values) => guinness.expect(values).toEqual([3]));
       });
     });
     
-    describe("Rx.Observable.flatMap", () {
-      it("transforms values and returns a new Observable", () {
+    guinness.describe("Rx.Observable.flatMap", () {
+      guinness.it("transforms values and returns a new Observable", () {
         return testObservable(observable.flatMap((int value) => new Rx.Observable.from([value * 2])),
-          expectation: (values) => expect(values).toEqual([2, 4, 6]));
+          expectation: (values) => guinness.expect(values).toEqual([2, 4, 6]));
       });
     });
     
-    describe("Rx.Observable.flatMapLatest", () {
-      it("transforms values and returns a new Observable", () {
+    guinness.describe("Rx.Observable.flatMapLatest", () {
+      guinness.it("transforms values and returns a new Observable", () {
         return testObservable(observable.flatMapLatest((int value) => new Rx.Observable.from([value * 2])),
-          expectation: (values) => expect(values).toEqual([2, 4, 6]));
+          expectation: (values) => guinness.expect(values).toEqual([2, 4, 6]));
       });
     });
   });
