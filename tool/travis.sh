2c91d7cc3a12d5fe375a5fd63c7bd0c79f9fe1e6
Frank Pepermans <frank@igindo.com>
2015-10-24 00:55:47 +0100

    travis update

---
+++ tool/travis.sh
@@ -0,0 +1,15 @@
+# Fast fail the script on failures.
+set -e
+
+# Run all tests.
+pub run test -p content-shell test/all_tests.html
+
+# Install dart_coveralls; gather and send coverage data.
+if [ "$REPO_TOKEN" ]; then
+  pub global activate dart_coveralls
+  pub global run dart_coveralls report \
+    --token $REPO_TOKEN \
+    --retry 2 \
+    --exclude-test-files \
+    test/all_tests.dart
+fi
