2d88247b9ca3aa169ae7cc7d4114fe1de19436e9
Kevin Moore <kevmoo@google.com>
2015-11-19 12:44:13 -0800

    fixes to allow pkg/test running tests

--- test/all_tests.dart
+++ test/all_tests.dart
@@ -4,17 +4,12 @@ library test.all;
 
 import 'package:test/test.dart' as test;
 
-import 'observable_tests.dart' as observable_tests;
-import 'scheduler_tests.dart' as scheduler_tests;
-import 'subject_tests.dart' as subject_tests;
+import 'observable_test.dart' as observable_tests;
+import 'scheduler_test.dart' as scheduler_tests;
+import 'subject_test.dart' as subject_tests;
 
 void main() {
   observable_tests.main();
   scheduler_tests.main();
   subject_tests.main();
 }
-
-
-
-
-
