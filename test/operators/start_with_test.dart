db6911342ee9e85d22ed8fd6385e90f14dafe8da
Frank Pepermans <frank@igindo.com>
2016-01-11 23:40:37 +0100

    added tap and startWith

---
+++ test/operators/start_with_test.dart
@@ -0,0 +1,55 @@
+library rx.test.operators.start_with;
+
+import 'dart:async';
+
+import 'package:test/test.dart';
+import 'package:rxdart/rx.dart' as rx;
+
+Stream _getStream() => new Stream<int>.fromIterable(const <int>[1, 2, 3, 4]);
+
+Stream _getErroneousStream() {
+  StreamController<num> controller = new StreamController<num>();
+
+  new Timer(const Duration(milliseconds: 100), () => controller.add(1));
+  new Timer(const Duration(milliseconds: 200), () => controller.add(2));
+  new Timer(const Duration(milliseconds: 300), () => controller.add(3));
+  new Timer(const Duration(milliseconds: 400), () {
+    controller.add(100 / 0); // throw!!!
+    controller.close();
+  });
+
+  return controller.stream;
+}
+
+void main() {
+  test('rx.Observable.startWith', () async {
+    const List<int> expectedOutput = const <int>[5, 1, 2, 3, 4];
+    int count = 0;
+
+    rx.observable(_getStream())
+        .startWith(5)
+        .listen(expectAsync((int result) {
+      expect(expectedOutput[count++], result);
+    }, count: expectedOutput.length));
+  });
+
+  test('rx.Observable.startWith.asBroadcastStream', () async {
+    Stream<int> observable = rx.observable(_getStream().asBroadcastStream())
+        .startWith(5);
+
+    // listen twice on same stream
+    observable.listen((_) {});
+    observable.listen((_) {});
+    // code should reach here
+    expect(true, true);
+  });
+
+  test('rx.Observable.startWith.error.shouldThrow', () async {
+    Stream<int> observableWithError = rx.observable(_getErroneousStream())
+        .startWith(5);
+
+    observableWithError.listen((_) => {}, onError: (e, s) {
+      expect(true, true);
+    });
+  });
+}
---
+++ test/operators/tap_test.dart
@@ -0,0 +1,65 @@
+library rx.test.operators.tap;
+
+import 'dart:async';
+
+import 'package:test/test.dart';
+import 'package:rxdart/rx.dart' as rx;
+
+Stream _getStream() {
+  StreamController<int> controller = new StreamController<int>();
+
+  new Timer(const Duration(milliseconds: 100), () => controller.add(1));
+  new Timer(const Duration(milliseconds: 200), () => controller.add(2));
+  new Timer(const Duration(milliseconds: 300), () => controller.add(3));
+  new Timer(const Duration(milliseconds: 400), () {
+    controller.add(4);
+    controller.close();
+  });
+
+  return controller.stream;
+}
+
+Stream _getErroneousStream() {
+  StreamController<num> controller = new StreamController<num>();
+
+  new Timer(const Duration(milliseconds: 100), () => controller.add(1));
+  new Timer(const Duration(milliseconds: 200), () => controller.add(2));
+  new Timer(const Duration(milliseconds: 300), () => controller.add(3));
+  new Timer(const Duration(milliseconds: 400), () {
+    controller.add(100 / 0); // throw!!!
+    controller.close();
+  });
+
+  return controller.stream;
+}
+
+void main() {
+  test('rx.Observable.tap', () async {
+    const List<int> expectedOutput = const <int>[1, 2, 3, 4];
+    int count = 0;
+
+    rx.observable(_getStream())
+        .tap((int value) => expect(expectedOutput[count++], value))
+        .listen((_) {});
+  });
+
+  test('rx.Observable.tap.asBroadcastStream', () async {
+    Stream<int> observable = rx.observable(_getStream().asBroadcastStream())
+        .tap((_) {});
+
+    // listen twice on same stream
+    observable.listen((_) {});
+    observable.listen((_) {});
+    // code should reach here
+    expect(true, true);
+  });
+
+  test('rx.Observable.tap.error.shouldThrow', () async {
+    Stream<int> observableWithError = rx.observable(_getErroneousStream())
+        .tap((_) {});
+
+    observableWithError.listen((_) => {}, onError: (e, s) {
+      expect(true, true);
+    });
+  });
+}
