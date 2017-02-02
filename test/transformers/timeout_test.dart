ed4041797decf478445169f359ba566de753a2e2
Igindo\FrankPepermans <frank@igindo.com>
2017-02-02 15:04:23 +0100

    fix test not closing in time

--- test/transformers/timeout_test.dart
+++ test/transformers/timeout_test.dart
@@ -5,13 +5,16 @@ import 'package:rxdart/rxdart.dart';
 
 void main() {
   test('rx.Observable.timeout', () async {
+    StreamSubscription<int> subscription;
+
     Stream<int> observable = new Observable<int>.fromFuture(
-            new Future<int>.delayed(new Duration(days: 1), () => 1))
+            new Future<int>.delayed(new Duration(milliseconds: 30), () => 1))
         .timeout(new Duration(milliseconds: 1));
 
-    observable.listen((_) {},
+    subscription = observable.listen((_) {},
         onError: expectAsync2((dynamic e, dynamic s) {
           expect(e is TimeoutException, isTrue);
+          subscription.cancel();
         }, count: 1));
   });
 }
