e284eb6b44fac99551829df69a6876c3a7022725
HVDK <hans@dotdotcommadot.com>
2017-02-08 14:36:31 +0100

    Implemented comments on PR

--- web/konami/konamicode.dart
+++ web/konami/konamicode.dart
@@ -1,4 +1,3 @@
-import 'dart:async';
 import 'dart:html';
 
 import 'package:collection/collection.dart';
@@ -6,23 +5,25 @@ import 'package:rxdart/rxdart.dart';
 
 void main() {
 
-  // The Konami Code: UP, UP, DOWN, DOWN, LEFT, RIGHT, LEFT, RIGHT, B, A
-  const codes = const <int>[38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+  const up = 38;
+  const down = 40;
+  const left = 37;
+  const right = 39;
+  const a = 65;
+  const b = 66;
+
+  /// The Konami Code: UP, UP, DOWN, DOWN, LEFT, RIGHT, LEFT, RIGHT, B, A
+  const konamiKeyCodes = const <int>[up, up, down, down, left, right, left, right, b, a];
   final result = querySelector('#result');
-  final keyboardInput$ctrl = new StreamController<KeyboardEvent>();
-  final keyboardInput$ = new Observable(keyboardInput$ctrl.stream);
 
-  // Listen to the DOM for the 'keyup' event
-  // and it to the streamController each time it occurs
-  document.addEventListener('keyup', (event) => keyboardInput$ctrl.add(event));
-
-  keyboardInput$
-    // Use map() to get the keyCode
+  /// Wrap the `document.onKeyUp` stream so we can use the methods on `Observable
+  new Observable(document.onKeyUp)
+    /// Use map() to get the keyCode
     .map((event) => event.keyCode)
-    // Use bufferWithCount() to remember the last 10 keyCodes
+    /// Use bufferWithCount() to remember the last 10 keyCodes
     .bufferWithCount(10, 1)
-    // Use where() to check for matching values
-    .where((list) => const IterableEquality<int>().equals(list, codes))
-    // Use listen() to display the result
+    /// Use where() to check for matching values
+    .where((lastTenKeyCodes) => const IterableEquality<int>().equals(lastTenKeyCodes, konamiKeyCodes))
+    /// Use listen() to display the result
     .listen((_) => result.innerHtml = 'KONAMI!');
 }
