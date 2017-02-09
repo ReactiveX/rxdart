0291ab8a13398d61ba85b1feef0e1ad2a5c3b086
Igindo\FrankPepermans <frank@igindo.com>
2017-02-09 10:06:23 +0100

    simplified the example

--- examples/bin/fibonacci.dart
+++ examples/bin/fibonacci.dart
@@ -19,23 +19,22 @@ void main(List<String> arguments) {
   /// read the command line argument, if none provided, default to 10
   var n = (arguments.length == 1) ? int.parse(arguments.first) : 10;
 
-  /// the first 2 fibonacci numbers
-  const seq1 = const IndexedPair(1, 1, 1);
-  const seq2 = const IndexedPair(1, 1, 2);
-
-  /// initial n[1,2] values
-  Iterable<IndexedPair> start =
-      n == 0 ? const [] : n == 1 ? const [seq1] : const [seq1, seq2];
-
-  /// given that the first 2 numbers in the sequence don't need calculation...
-  var m = n >= 2 ? n - 2 : 0;
+  /// seed value: this value will be used as the
+  /// starting value for the [scan] method
+  const seed = const IndexedPair(1, 1, 0);
 
   Observable
-      .range(0, m)
-      .skip(1)
-      .scan((seq, _, __) => new IndexedPair.next(seq), seq2)
-      .startWithMany(start)
-      .map((seq) => seq.stringify())
+
+      /// amount of numbers to compute
+      .range(1, n)
+
+      /// accumulator: computes a new accumulated
+      /// value each time a [Stream] event occurs
+      /// in this case, the accumulated value is always
+      /// the latest Fibonacci number
+      .scan((seq, _, __) => new IndexedPair.next(seq), seed)
+
+      /// finally, print the output
       .listen(print, onDone: () => print('done!'));
 }
 
@@ -44,8 +43,8 @@ class IndexedPair {
 
   const IndexedPair(this.n1, this.n2, this.index);
 
-  factory IndexedPair.next(IndexedPair prev) =>
-      new IndexedPair(prev.n2, prev.n1 + prev.n2, prev.index + 1);
+  factory IndexedPair.next(IndexedPair prev) => new IndexedPair(
+      prev.n2, prev.index <= 1 ? prev.n1 : prev.n1 + prev.n2, prev.index + 1);
 
-  String stringify() => '$index: $n2';
+  String toString() => '$index: $n2';
 }
