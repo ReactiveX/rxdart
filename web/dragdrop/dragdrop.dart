6a4913d05000324843c45d84d01709276e6033ae
Hans Van den Keybus <hans@dotdotcommadot.com>
2017-02-10 13:36:24 +0100

    Updates DragDrop example to use document.onKeyUp stream (#58)
    * Uses document.onMouseUp instead of custom streamController
    
    * Fixes Typo
    
    * Implements Point instead of Map<String, int> + general cleanup

--- web/dragdrop/dragdrop.dart
+++ web/dragdrop/dragdrop.dart
@@ -1,55 +1,35 @@
 import 'dart:html';
-import 'dart:async';
-
 import 'package:rxdart/rxdart.dart';
 
-Observable<MouseEvent> _getMouseObservable(String mouseEvent) {
-
-  // Instantiate a StreamController
-  final StreamController<MouseEvent> controller = new StreamController<MouseEvent>.broadcast();
-
-  // Listen to the DOM for mouseEvents
-  // and add each event to the streamController
-  document.body.addEventListener(mouseEvent, (Event event) => controller.add(event));
-
-  // Print the mouseEvent each time a stream gets cancelled
-  controller.onCancel = () {
-    print('CANCELED ' + mouseEvent);
-  };
-
-  // Convert the streamController's stream to an Observable
-  // and return it
-  return new Observable<MouseEvent>(controller.stream);
-}
+// Side note: To maintain readability, this example was not formatted using dart_fmt.
 
 void main() {
-  final Element dragTarget = querySelector('#dragTarget');
-
-  Observable<MouseEvent> mouseUp = _getMouseObservable('mouseup');
-  Observable<MouseEvent> mouseMove = _getMouseObservable('mousemove');
-  Observable<MouseEvent> mouseDown = _getMouseObservable('mousedown');
+  final dragTarget = querySelector('#dragTarget');
+  final mouseUp = new Observable<MouseEvent>(document.onMouseUp);
+  final mouseMove = new Observable<MouseEvent>(document.onMouseMove);
+  final mouseDown = new Observable<MouseEvent>(document.onMouseDown);
 
   mouseDown
-    // Use map() to Calculate the left and top properties on mouseDown
-    .map((MouseEvent e) => <String, int>{
-      'left': e.client.x - dragTarget.offset.left,
-      'top': e.client.y - dragTarget.offset.top
-    })
-    // Use flatMapLatest() to get the mouse position on each mouseMove
-    .flatMapLatest((Map<String, int> startPos) {
+    /// Use map() to calculate the left and top properties on mouseDown
+    .map((event) => new Point<num>(
+      event.client.x - dragTarget.offset.left,
+      event.client.y - dragTarget.offset.top
+    ))
+    /// Use flatMapLatest() to get the mouse position on each mouseMove
+    .flatMapLatest((startPosition) {
 
       return mouseMove
-        // Use map() to calculate the left and top properties on each mouseMove
-        .map((MouseEvent pos) => <String, int>{
-          'left': pos.client.x - startPos['left'],
-          'top': pos.client.y - startPos['top']
-        })
-        // Use takeUntil() to stop calculations when a mouseUp occurs
+        /// Use map() to calculate the left and top properties on each mouseMove
+        .map((event) => new Point<num>(
+          event.client.x - startPosition.x,
+          event.client.y - startPosition.y
+        ))
+        /// Use takeUntil() to stop calculations when a mouseUp occurs
         .takeUntil(mouseUp);
     })
-    // Use listen() to update the position of the dragTarget
-    .listen((Map<String, int> e) {
-      dragTarget.style.top = '${e['top']}px';
-      dragTarget.style.left = '${e['left']}px';
+    /// Use listen() to update the position of the dragTarget
+    .listen((position) {
+      dragTarget.style.left = '${position.x}px';
+      dragTarget.style.top = '${position.y}px';
     });
 }
