b6b5993be6d7dfdb26f41d35907813e5b9187230
HVDK <hans@dotdotcommadot.com>
2017-02-10 13:34:37 +0100

    Reverts ‘///‘ to ‘//‘

--- web/konami/konamicode.dart
+++ web/konami/konamicode.dart
@@ -4,7 +4,6 @@ import 'package:collection/collection.dart';
 import 'package:rxdart/rxdart.dart';
 
 void main() {
-
   const konamiKeyCodes = const <int>[
     KeyCode.UP,
     KeyCode.UP,
@@ -19,15 +18,15 @@ void main() {
   ];
 
   final result = querySelector('#result');
+  final keyUp = new Observable<KeyboardEvent>(document.onKeyUp);
 
-  /// Wrap the `document.onKeyUp` stream so we can use the methods on `Observable`
-  new Observable(document.onKeyUp)
-    /// Use map() to get the keyCode
+  keyUp
+    // Use map() to get the keyCode
     .map((event) => event.keyCode)
-    /// Use bufferWithCount() to remember the last 10 keyCodes
+    // Use bufferWithCount() to remember the last 10 keyCodes
     .bufferWithCount(10, 1)
-    /// Use where() to check for matching values
+    // Use where() to check for matching values
     .where((lastTenKeyCodes) => const IterableEquality<int>().equals(lastTenKeyCodes, konamiKeyCodes))
-    /// Use listen() to display the result
+    // Use listen() to display the result
     .listen((_) => result.innerHtml = 'KONAMI!');
 }
