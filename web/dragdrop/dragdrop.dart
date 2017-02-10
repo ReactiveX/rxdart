54bed86b28dfb4b0c5b4e1b9198c0e0cb0b1778d
HVDK <hans@dotdotcommadot.com>
2017-02-10 13:29:29 +0100

    Implements Point instead of Map<String, int> + general cleanup

--- web/dragdrop/dragdrop.dart
+++ web/dragdrop/dragdrop.dart
@@ -1,34 +1,35 @@
 import 'dart:html';
 import 'package:rxdart/rxdart.dart';
 
-void main() {
+// Side note: To maintain readability, this example was not formatted using dart_fmt.
 
+void main() {
   final dragTarget = querySelector('#dragTarget');
-  final mouseUp = new Observable(document.onMouseUp);
-  final mouseMove = new Observable(document.onMouseMove);
-  final mouseDown = new Observable(document.onMouseDown);
+  final mouseUp = new Observable<MouseEvent>(document.onMouseUp);
+  final mouseMove = new Observable<MouseEvent>(document.onMouseMove);
+  final mouseDown = new Observable<MouseEvent>(document.onMouseDown);
 
   mouseDown
     /// Use map() to calculate the left and top properties on mouseDown
-    .map((MouseEvent e) => <String, int>{
-      'left': e.client.x - dragTarget.offset.left,
-      'top': e.client.y - dragTarget.offset.top
-    })
+    .map((event) => new Point<num>(
+      event.client.x - dragTarget.offset.left,
+      event.client.y - dragTarget.offset.top
+    ))
     /// Use flatMapLatest() to get the mouse position on each mouseMove
-    .flatMapLatest((Map<String, int> startPos) {
+    .flatMapLatest((startPosition) {
 
       return mouseMove
         /// Use map() to calculate the left and top properties on each mouseMove
-        .map((MouseEvent pos) => <String, int>{
-          'left': pos.client.x - startPos['left'],
-          'top': pos.client.y - startPos['top']
-        })
+        .map((event) => new Point<num>(
+          event.client.x - startPosition.x,
+          event.client.y - startPosition.y
+        ))
         /// Use takeUntil() to stop calculations when a mouseUp occurs
         .takeUntil(mouseUp);
     })
     /// Use listen() to update the position of the dragTarget
-    .listen((Map<String, int> e) {
-      dragTarget.style.top = '${e['top']}px';
-      dragTarget.style.left = '${e['left']}px';
+    .listen((position) {
+      dragTarget.style.left = '${position.x}px';
+      dragTarget.style.top = '${position.y}px';
     });
 }
