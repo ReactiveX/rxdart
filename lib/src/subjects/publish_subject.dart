194af34e68456d265e804d1840ceecf4bdbb19cc
Brian Egan <brian@brianegan.com>
2017-08-19 01:28:55 +0100

    Publish subject (#105)
    * PublishSubject added.
    
    * Added PublishSubject tests.
    
    * Minor comment revisions
    
    * Refactor Subject with a common base class

---
+++ lib/src/subjects/publish_subject.dart
@@ -0,0 +1,44 @@
+import 'dart:async';
+import 'package:rxdart/src/observable.dart';
+import 'package:rxdart/src/subjects/subject.dart';
+
+/// Exactly like a normal broadcast StreamController with one exception:
+/// `stream` returns an `Observable` instead of a `Stream`.
+///
+/// This Subject allows sending data, error and done events to the listener.
+///
+/// PublishSubject is, by default, a broadcast (aka hot) controller, in order
+/// to fulfill the Rx Subject contract. This means the Subject's `stream` can
+/// be listened to multiple times.
+///
+/// ### Example
+///
+///     final subject = new PublishSubject<int>();
+///
+///     // observer1 will receive all data and done events
+///     subject.stream.listen(observer1);
+///     subject.add(1);
+///     subject.add(2);
+///
+///     // observer2 will only receive 3 and done event
+///     subject.stream.listen(observe2);
+///     subject.add(3);
+///     subject.close();
+class PublishSubject<T> extends Subject<T> {
+  PublishSubject._(StreamController<T> controller, Observable<T> observable)
+      : super(controller, observable);
+
+  factory PublishSubject({void onListen(), onCancel(), bool sync: false}) {
+    // ignore: close_sinks
+    final StreamController<T> controller = new StreamController<T>.broadcast(
+      onListen: onListen,
+      onCancel: onCancel,
+      sync: sync,
+    );
+
+    return new PublishSubject<T>._(
+      controller,
+      new Observable<T>(controller.stream),
+    );
+  }
+}
---
+++ lib/src/subjects/subject.dart
@@ -0,0 +1,138 @@
+import 'dart:async';
+import 'package:rxdart/src/observable.dart';
+
+/// The base for all Subjects. If you'd like to create a new Subject,
+/// extend from this class.
+///
+/// It handles all of the nitty-gritty details that conform to the
+/// StreamController spec and don't need to be repeated over and
+/// over.
+///
+/// Please see `PublishSubject` for the simplest example of how to
+/// extend from this class, or `BehaviorSubject` for a slightly more
+/// complex example.
+abstract class Subject<T> implements StreamController<T> {
+  final StreamController<T> controller;
+  final Observable<T> observable;
+  bool _isAddingStreamItems = false;
+
+  Subject(this.controller, this.observable);
+
+  @override
+  Observable<T> get stream => observable;
+
+  @override
+  StreamSink<T> get sink => controller.sink;
+
+  @override
+  ControllerCallback get onListen => controller.onListen;
+
+  @override
+  set onListen(void onListenHandler()) {
+    controller.onListen = onListenHandler;
+  }
+
+  @override
+  ControllerCallback get onPause =>
+      throw new UnsupportedError("Subjects do not support pause callbacks");
+
+  @override
+  set onPause(void onPauseHandler()) =>
+      throw new UnsupportedError("Subjects do not support pause callbacks");
+
+  @override
+  ControllerCallback get onResume =>
+      throw new UnsupportedError("Subjects do not support resume callbacks");
+
+  @override
+  set onResume(void onResumeHandler()) =>
+      throw new UnsupportedError("Subjects do not support resume callbacks");
+
+  @override
+  ControllerCancelCallback get onCancel => controller.onCancel;
+
+  @override
+  set onCancel(onCancelHandler()) {
+    controller.onCancel = onCancelHandler;
+  }
+
+  @override
+  bool get isClosed => controller.isClosed;
+
+  @override
+  bool get isPaused => controller.isPaused;
+
+  @override
+  bool get hasListener => controller.hasListener;
+
+  @override
+  Future<dynamic> get done => controller.done;
+
+  @override
+  void addError(Object error, [StackTrace stackTrace]) {
+    if (_isAddingStreamItems) {
+      throw new StateError(
+          "You cannot add an error while items are being added from addStream");
+    }
+
+    controller.addError(error, stackTrace);
+  }
+
+  @override
+  Future<dynamic> addStream(Stream<T> source, {bool cancelOnError: true}) {
+    if (_isAddingStreamItems) {
+      throw new StateError(
+          "You cannot add items while items are being added from addStream");
+    }
+
+    final Completer<T> completer = new Completer<T>();
+    _isAddingStreamItems = true;
+
+    source.listen((T event) {
+      _add(event);
+    }, onError: (dynamic e, dynamic s) {
+      controller.addError(e, s);
+
+      if (cancelOnError) {
+        _isAddingStreamItems = false;
+        completer.completeError(e);
+      }
+    }, onDone: () {
+      _isAddingStreamItems = false;
+      completer.complete();
+    }, cancelOnError: cancelOnError);
+
+    return completer.future;
+  }
+
+  @override
+  void add(T event) {
+    if (_isAddingStreamItems) {
+      throw new StateError(
+          "You cannot add items while items are being added from addStream");
+    }
+
+    _add(event);
+  }
+
+  void _add(T event) {
+    onAdd(event);
+
+    controller.add(event);
+  }
+
+  /// An extension point for sub-classes. Perform any side-effect / state
+  /// management you need to here, rather than overriding the `add` method
+  /// directly.
+  void onAdd(T event) {}
+
+  @override
+  Future<dynamic> close() {
+    if (_isAddingStreamItems) {
+      throw new StateError(
+          "You cannot close the subject while items are being added from addStream");
+    }
+
+    return controller.close();
+  }
+}
