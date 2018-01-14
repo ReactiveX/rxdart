629dcc01d2d72865bc79b97a892d0b591f180f3c
Brian Egan <brian@brianegan.com>
2018-01-14 12:43:12 +0100

    Update flutter example (#115)
    * Update Flutter example
    
    * Use new initialData from Flutter

--- example/flutter/github_search/lib/empty_result_widget.dart
+++ example/flutter/github_search/lib/empty_result_widget.dart
@@ -8,19 +8,28 @@ class EmptyResultWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new AnimatedOpacity(
-        duration: new Duration(milliseconds: 300),
-        opacity: isEmpty ? 1.0 : 0.0,
-        child: new Container(
-            alignment: FractionalOffset.center,
-            child: new Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  new Icon(Icons.warning,
-                      color: Colors.yellow[200], size: 80.0),
-                  new Container(
-                      padding: new EdgeInsets.only(top: 16.0),
-                      child: new Text("No results",
-                          style: new TextStyle(color: Colors.yellow[100])))
-                ])));
+      duration: new Duration(milliseconds: 300),
+      opacity: isEmpty ? 1.0 : 0.0,
+      child: new Container(
+        alignment: FractionalOffset.center,
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new Icon(
+              Icons.warning,
+              color: Colors.yellow[200],
+              size: 80.0,
+            ),
+            new Container(
+              padding: new EdgeInsets.only(top: 16.0),
+              child: new Text(
+                "No results",
+                style: new TextStyle(color: Colors.yellow[100]),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
