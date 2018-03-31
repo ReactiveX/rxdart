77ee9c74508159bedc9f25847ad8d18f49db7ad1
Igindo\FrankPepermans <frank@igindo.com>
2018-03-31 08:17:48 +0200

    skipUntil onDone fix and added tests

--- lib/src/transformers/skip_until.dart
+++ lib/src/transformers/skip_until.dart
@@ -31,6 +31,12 @@ class SkipUntilStreamTransformer<T, S> extends StreamTransformerBase<T, T> {
       StreamSubscription<S> otherSubscription;
       bool goTime = false;
 
+      void onDone() {
+        if (controller.isClosed) return;
+
+        controller.close();
+      }
+
       controller = new StreamController<T>(
           sync: true,
           onListen: () {
@@ -40,14 +46,17 @@ class SkipUntilStreamTransformer<T, S> extends StreamTransformerBase<T, T> {
               }
             },
                 onError: controller.addError,
-                onDone: controller.close,
+                onDone: onDone,
                 cancelOnError: cancelOnError);
 
             otherSubscription = otherStream.listen((_) {
               goTime = true;
 
               otherSubscription.cancel();
-            }, onError: controller.addError, cancelOnError: cancelOnError);
+            },
+                onError: controller.addError,
+                cancelOnError: cancelOnError,
+                onDone: onDone);
           },
           onPause: ([Future<dynamic> resumeSignal]) =>
               subscription.pause(resumeSignal),
