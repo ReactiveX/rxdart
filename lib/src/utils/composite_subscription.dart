4869292de82f35c10d9859d4abc144e6feeee4d7
Mikhail Levchenko <themishkun@yandex-team.ru>
2018-10-07 12:18:27 +0300

    Add CompositeSubscription class and docs
    This commit add CompositeSubscription util class as described in issue #183
    CompositeSubscription is a container for subscriptions making easy to cancel all added subscriptions at once.
    This Composite implementation is ported from RxJava's implementation of the same concept

---
+++ lib/src/utils/composite_subscription.dart
@@ -0,0 +1,68 @@
+
+import 'dart:async';
+
+/**
+ * Acts as a container for multiple subscriptions that can be canceled at once 
+ * e.g. view subcriptions in Flutter that need to be canceled on view disposal
+ * 
+ * Can be cleared or disposed. When disposed, cannot be used again.
+ * ### Example
+ *      // init your subscriptions
+ *      composite.add(observable1.listen(listener1))
+ *            ..add(observable2.listen(listener1))
+ *            ..add(observable3.listen(listener1));
+ * 
+ *      // clear them all at once
+ *      composite.clear();
+ */
+class CompositeSubscription {
+
+  bool _isDisposed = false;
+
+  final List<StreamSubscription> _subscriptionsList = new List<StreamSubscription>();
+
+  /**
+   * Checks if this composite is disposed. If it is, the composite can't be used again
+   * and will throw an error if you try to add more subscriptions to it.
+   */
+  bool get isDisposed => _isDisposed;
+
+  /**
+   * Adds new subscription to this composite.
+   * 
+   * Throws an exception if this composite was disposed
+   */
+  CompositeSubscription add(StreamSubscription subscription) {
+    if (isDisposed) throw("This composite was disposed, try to use new instance instead");
+    _subscriptionsList.add(subscription);
+    return this;
+  }
+
+  /**
+   * Removes subscription from this composite.
+   */
+  CompositeSubscription remove(StreamSubscription subscription) {
+    _subscriptionsList.remove(subscription);
+    return this;
+  }
+  
+  /**
+   * Cancels all subscriptions added to this composite. Clears subscriptions collection.
+   * 
+   * This composite can be reused after calling this method.
+   */
+  void clear() {
+    _subscriptionsList.forEach((subscription) => subscription.cancel());
+    _subscriptionsList.clear();
+  }
+
+  /**
+   * Cancels all subscriptions added to this composite. Disposes this.
+   * 
+   * This composite can't be reused after calling this method.
+   */
+  void dispose() {
+    clear();
+    _isDisposed = true;
+  }
+}
