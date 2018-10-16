a76ab7924d3b7d9e37e23be18d2baf9b70afb9e9
Mikhail Levchenko <mishkun.lev@gmail.com>
2018-10-16 12:36:24 +0300

    Add CompositeSubscription container to manage multiple subscriptions at once (#191)
    * Add CompositeSubscription class and docs
    
    This commit add CompositeSubscription util class as described in issue #183
    CompositeSubscription is a container for subscriptions making easy to cancel all added subscriptions at once.
    This Composite implementation is ported from RxJava's implementation of the same concept
    
    * Add test for CompositeSubscription
    
    This test checks three properties of composite:
     - it should cancel every added subscription on clear and dispose
     - if removed from composite, subscription won't cancel on clear and dispose
     - it should throw an error instead of adding subscriprtion if it was disposed
    
    * Add CompositeSubscription file to export
    
    * Format the PR with the `dartfmt` tool
    
    * Replace /** */ style docs with /// to adhere dart guidelines
    
    * Specify explicit <dynamic> type annotations
    
    * Fix type annotations in tests
    
    * Register CompositeSubscription in test runner
    
    CompositeSubscription tests weren't running on the CI because they weren't included in the test suite
    
    * Make composite cancel subscription on remove
    This is more compliant to RxJava CompositeSubscription behaviour
    
    * Remove Java-like fluent interface
    
    In Dart, method cascades can make auto-fluent interfaces for free,
    So methods like `add` can now return the subscription added, not the composite
    This makes API more versatile and adaptable for more usecases

---
+++ test/observables/composite_subscription_test.dart
@@ -0,0 +1,58 @@
+import 'dart:async';
+
+import 'package:rxdart/rxdart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('CompositeSubscription', () {
+    test('should cancel all subscriptions on clear()', () {
+      final Observable<int> observable =
+          Observable<int>.fromIterable(<int>[1, 2, 3]).shareValue();
+      final CompositeSubscription composite = CompositeSubscription();
+
+      composite
+        ..add(observable.listen(null))
+        ..add(observable.listen(null))
+        ..add(observable.listen(null));
+
+      composite.clear();
+
+      expect(observable, neverEmits(anything));
+    });
+    test('should cancel all subscriptions on dispose()', () {
+      final Observable<int> observable =
+          Observable<int>.fromIterable(<int>[1, 2, 3]).shareValue();
+      final CompositeSubscription composite = CompositeSubscription();
+
+      composite
+        ..add(observable.listen(null))
+        ..add(observable.listen(null))
+        ..add(observable.listen(null));
+
+      composite.dispose();
+
+      expect(observable, neverEmits(anything));
+    });
+    test('should throw exception if trying to add subscription to disposed composite', () {
+      final Observable<int> observable =
+          Observable<int>.fromIterable(<int>[1, 2, 3]).shareValue();
+      final CompositeSubscription composite = CompositeSubscription();
+
+      composite.dispose();
+
+      expect(() => composite.add(observable.listen(null)), throwsA(anything));
+    });
+    test('should cancel subscription on if it is removed from composite', () {
+      const int value = 1;
+      final Observable<int> observable =
+          Observable<int>.fromIterable(<int>[value]).shareValue();
+      final CompositeSubscription composite = CompositeSubscription();
+      final StreamSubscription<int> subscription = observable.listen(null);
+
+      composite.add(subscription);
+      composite.remove(subscription);
+
+      expect(observable, neverEmits(anything));
+    });
+  });
+}
