67f3bac0dfbaac9734914e46aca982260dd0923d
Brian Egan <brian@brianegan.com>
2019-08-21 21:22:50 +0200

    Update sample to latest Flutter stable (#319)
    * Update sample to latest Flutter stable
    
    * Don't run xvfb before we run our tests

--- example/flutter/github_search/lib/search_intro_widget.dart
+++ example/flutter/github_search/lib/search_intro_widget.dart
@@ -1,32 +1,26 @@
 import 'package:flutter/material.dart';
 
 class SearchIntro extends StatelessWidget {
-  final bool visible;
-
-  const SearchIntro({Key key, this.visible}) : super(key: key);
+  const SearchIntro({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return AnimatedOpacity(
-      duration: Duration(milliseconds: 300),
-      opacity: visible ? 1.0 : 0.0,
-      child: Container(
-        alignment: FractionalOffset.center,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Icon(Icons.info, color: Colors.green[200], size: 80.0),
-            Container(
-              padding: EdgeInsets.only(top: 16.0),
-              child: Text(
-                "Enter a search term to begin",
-                style: TextStyle(
-                  color: Colors.green[100],
-                ),
+    return Container(
+      alignment: FractionalOffset.center,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Icon(Icons.info, color: Colors.green[200], size: 80.0),
+          Container(
+            padding: EdgeInsets.only(top: 16.0),
+            child: Text(
+              "Enter a search term to begin",
+              style: TextStyle(
+                color: Colors.green[100],
               ),
-            )
-          ],
-        ),
+            ),
+          )
+        ],
       ),
     );
   }
