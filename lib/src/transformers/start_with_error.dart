6236c8a5c039a7df3526683c548ec045b2f71069
Igindo\FrankPepermans <frank@igindo.com>
2020-03-07 13:42:03 +0100

    simplified startWith

--- lib/src/transformers/start_with_error.dart
+++ lib/src/transformers/start_with_error.dart
@@ -1,54 +1,5 @@
 import 'dart:async';
 
-import 'package:rxdart/src/utils/forwarding_sink.dart';
-import 'package:rxdart/src/utils/forwarding_stream.dart';
-
-class _StartWithErrorStreamSink<S> implements ForwardingSink<S> {
-  final EventSink<S> _outputSink;
-  final Object _e;
-  final StackTrace _st;
-  var _isFirstEventAdded = false;
-
-  _StartWithErrorStreamSink(this._outputSink, this._e, this._st);
-
-  @override
-  void add(S data) {
-    _addFirstEvent();
-    _outputSink.add(data);
-  }
-
-  @override
-  void addError(e, [st]) {
-    _addFirstEvent();
-    _outputSink.addError(e, st);
-  }
-
-  @override
-  void close() {
-    _addFirstEvent();
-    _outputSink.close();
-  }
-
-  @override
-  FutureOr onCancel() {}
-
-  @override
-  void onListen() => scheduleMicrotask(_addFirstEvent);
-
-  @override
-  void onPause() {}
-
-  @override
-  void onResume() {}
-
-  void _addFirstEvent() {
-    if (!_isFirstEventAdded) {
-      _isFirstEventAdded = true;
-      _outputSink.addError(_e, _st);
-    }
-  }
-}
-
 /// Prepends an error to the source Stream.
 ///
 /// ### Example
@@ -69,11 +20,12 @@ class StartWithErrorStreamTransformer<S> extends StreamTransformerBase<S, S> {
 
   @override
   Stream<S> bind(Stream<S> stream) {
-    final forwardedStream = forwardStream<S>(stream);
+    final generator = () async* {
+      throw error;
 
-    return Stream.eventTransformed(
-        forwardedStream.stream,
-        (sink) => forwardedStream
-            .connect(_StartWithErrorStreamSink<S>(sink, error, stackTrace)));
+      yield* stream;
+    };
+
+    return stream.isBroadcast ? generator().asBroadcastStream() : generator();
   }
 }
