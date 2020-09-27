55bda97b479c9f0c8f784d2019d5d0f613f17356
Petrus Nguyen Thai Hoc <hoc081098@gmail.com>
2020-09-28 01:12:43 +0700

    ➕ Add Rx.fromCallable

---
+++ lib/src/streams/from_callable.dart
@@ -0,0 +1,64 @@
+import 'dart:async';
+
+/// Returns a Stream that, when listening to it, calls a function you specify
+/// and then emits the value returned from that function.
+///
+/// If result from invoking [callable] function:
+/// - Is a [Future]: when the future completes, this stream will fire one event, either
+///   data or error, and then close with a done-event.
+/// - Is a [T]: this stream emits a single data event and then completes with a done event.
+///
+/// A [FromCallableStream] is always a single-subscription Stream.
+/// This Stream is effectively equivalent to one created by
+/// `(() async* { yield await callable() }())` or `(() async* { yield callable(); }())`.
+///
+/// [ReactiveX doc](http://reactivex.io/documentation/operators/from.html)
+///
+/// ### Example
+///
+///     FromCallableStream(() => 'Value').listen(print); // prints Value
+///
+///     FromCallableStream(() async {
+///       await Future<void>.delayed(const Duration(seconds: 1));
+///       return 'Value';
+///     }).listen(print); // prints Value
+class FromCallableStream<T> extends Stream<T> {
+  Stream<T> _stream;
+
+  /// A function will be called at subscription time.
+  final FutureOr<T> Function() callable;
+
+  /// Construct a Stream that, when listening to it, calls a function you specify
+  /// and then emits the value returned from that function.
+  FromCallableStream(this.callable);
+
+  @override
+  bool get isBroadcast => false;
+
+  @override
+  StreamSubscription<T> listen(
+    void Function(T event) onData, {
+    Function onError,
+    void Function() onDone,
+    bool cancelOnError,
+  }) {
+    if (_stream == null) {
+      try {
+        final value = callable();
+
+        _stream = value is Future<T>
+            ? Stream.fromFuture(value)
+            : Stream.value(value as T);
+      } catch (e, s) {
+        _stream = Stream.error(e, s);
+      }
+    }
+
+    return _stream.listen(
+      onData,
+      onError: onError,
+      onDone: onDone,
+      cancelOnError: cancelOnError,
+    );
+  }
+}
