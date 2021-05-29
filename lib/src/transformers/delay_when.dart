3bc38cbe45176e331374e8290e2f395157d1afe7
Petrus Nguyen Thai Hoc <hoc081098@gmail.com>
2021-05-29 13:31:54 +0700

    wip

--- lib/src/transformers/delay_when.dart
+++ lib/src/transformers/delay_when.dart
@@ -1,11 +1,7 @@
 import 'dart:async';
 
-import 'package:rxdart/rxdart.dart';
-import 'package:rxdart/src/rx.dart';
 import 'package:rxdart/src/utils/forwarding_sink.dart';
 import 'package:rxdart/src/utils/forwarding_stream.dart';
-
-import 'ignore_elements.dart';
 
 class _DelayWhenStreamSink<T> implements ForwardingSink<T, T> {
   final Stream<void> Function(T) itemDelaySelector;
@@ -61,12 +57,6 @@ class _DelayWhenStreamSink<T> implements ForwardingSink<T, T> {
   void onResume(EventSink<T> sink) => subscriptions.forEach((s) => s.resume());
 }
 
-class _DelayListenStreamSink<T> implements ForwardingSink<T, T> {
-  final Stream<void> subscriptionDelay;
-
-  _DelayListenStreamSink(this.subscriptionDelay);
-}
-
 /// Delays the emission of items from the source [Stream] by a given time span
 /// determined by the emissions of another [Stream].
 ///
@@ -79,21 +69,14 @@ class _DelayListenStreamSink<T> implements ForwardingSink<T, T> {
 ///       .listen(print); // [after 1 second] prints 1 [after 1 second] prints 2 [after 1 second] prints 3
 class DelayWhenStreamTransformer<T> extends StreamTransformerBase<T, T> {
   final Stream<void> Function(T) itemDelaySelector;
-  final Stream<void>? subscriptionDelay;
 
   /// Constructs a [StreamTransformer] which delays the emission of items
   /// from the source [Stream] by a given time span determined by the emissions of another [Stream].
-  DelayWhenStreamTransformer(this.itemDelaySelector, this.subscriptionDelay);
+  DelayWhenStreamTransformer(this.itemDelaySelector);
 
   @override
-  Stream<T> bind(Stream<T> stream) {
-    if (subscriptionDelay != null) {
-      stream = forwardStream(stream, _DelayListenStreamSink(subscriptionDelay));
-    }
-    print('>>' +stream.toString());
-    print('>>' +stream.isBroadcast.toString());
-    return forwardStream(stream, _DelayWhenStreamSink(itemDelaySelector));
-  }
+  Stream<T> bind(Stream<T> stream) =>
+      forwardStream(stream, _DelayWhenStreamSink(itemDelaySelector));
 }
 
 /// Extends the Stream class with the ability to delay events being emitted.
@@ -106,11 +89,6 @@ extension DelayWhenExtension<T> on Stream<T> {
   /// The source element is emitted on the output Stream only when the "duration" Stream
   /// emits a data or done event.
   ///
-  /// Optionally, `delayWhen` takes a second argument `listenDelay`. When `listenDelay`
-  /// emits its first data or done event, the source Stream is listen to.
-  /// If `listenDelay` is not provided, `delayWhen` will listen to the source Stream
-  /// as soon as the output Stream is listen.
-  ///
   /// [Interactive marble diagram](http://rxmarbles.com/#delayWhen)
   ///
   /// ### Example
@@ -118,15 +96,6 @@ extension DelayWhenExtension<T> on Stream<T> {
   ///     Stream.fromIterable([1, 2, 3])
   ///       .delayWhen((i) => Rx.timer(null, Duration(seconds: i)))
   ///       .listen(print); // [after 1 second] prints 1 [after 1 second] prints 2 [after 1 second] prints 3
-  Stream<T> delayWhen(Stream<void> Function(T) itemDelaySelector,
-          [Stream<void>? listenDelay]) =>
-      transform(DelayWhenStreamTransformer(itemDelaySelector, listenDelay));
-}
-
-void main() {
-  var delayWhen = PublishSubject<int>().delayWhen((p0) => Stream.value(p0), Rx.timer(0, Duration.zero));
-  print('<< $delayWhen');
-  delayWhen.listen(print);
-  delayWhen.listen(print);
-  print(delayWhen.isBroadcast);
+  Stream<T> delayWhen(Stream<void> Function(T) itemDelaySelector) =>
+      transform(DelayWhenStreamTransformer(itemDelaySelector));
 }
