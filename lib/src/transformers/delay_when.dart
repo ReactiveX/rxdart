498c0c7ae357d96c7e5f5814d75c969f61cba508
Petrus Nguyen Thai Hoc <hoc081098@gmail.com>
2021-08-07 00:10:11 +0700

    fix

--- lib/src/transformers/delay_when.dart
+++ lib/src/transformers/delay_when.dart
@@ -3,7 +3,7 @@ import 'dart:async';
 import 'package:rxdart/src/utils/forwarding_sink.dart';
 import 'package:rxdart/src/utils/forwarding_stream.dart';
 
-class _DelayWhenStreamSink<T> implements ForwardingSink<T, T> {
+class _DelayWhenStreamSink<T> extends ForwardingSink<T, T> {
   final Stream<void> Function(T) itemDelaySelector;
   final subscriptions = <StreamSubscription<void>>[];
   var closed = false;
@@ -11,7 +11,7 @@ class _DelayWhenStreamSink<T> implements ForwardingSink<T, T> {
   _DelayWhenStreamSink(this.itemDelaySelector);
 
   @override
-  void add(EventSink<T> sink, T data) {
+  void onData(T data) {
     final subscription =
         itemDelaySelector(data).take(1).listen(null, onError: sink.addError);
 
@@ -28,11 +28,10 @@ class _DelayWhenStreamSink<T> implements ForwardingSink<T, T> {
   }
 
   @override
-  void addError(EventSink<T> sink, Object error, StackTrace st) =>
-      sink.addError(error, st);
+  void onError(Object error, StackTrace st) => sink.addError(error, st);
 
   @override
-  void close(EventSink<T> sink) {
+  void onDone() {
     closed = true;
     if (subscriptions.isEmpty) {
       sink.close();
@@ -40,7 +39,7 @@ class _DelayWhenStreamSink<T> implements ForwardingSink<T, T> {
   }
 
   @override
-  FutureOr<void> onCancel(EventSink<T> sink) {
+  FutureOr<void> onCancel() {
     if (subscriptions.isNotEmpty) {
       return Future.wait(subscriptions.map((s) => s.cancel()))
           .whenComplete(() => subscriptions.clear());
@@ -48,13 +47,13 @@ class _DelayWhenStreamSink<T> implements ForwardingSink<T, T> {
   }
 
   @override
-  void onListen(EventSink<T> sink) {}
+  void onListen() {}
 
   @override
-  void onPause(EventSink<T> sink) => subscriptions.forEach((s) => s.pause());
+  void onPause() => subscriptions.forEach((s) => s.pause());
 
   @override
-  void onResume(EventSink<T> sink) => subscriptions.forEach((s) => s.resume());
+  void onResume() => subscriptions.forEach((s) => s.resume());
 }
 
 /// Delays the emission of items from the source [Stream] by a given time span
@@ -77,7 +76,7 @@ class DelayWhenStreamTransformer<T> extends StreamTransformerBase<T, T> {
 
   @override
   Stream<T> bind(Stream<T> stream) =>
-      forwardStream(stream, _DelayWhenStreamSink(itemDelaySelector));
+      forwardStream(stream, () => _DelayWhenStreamSink(itemDelaySelector));
 }
 
 /// Extends the Stream class with the ability to delay events being emitted.
