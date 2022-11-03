871cf6ad0520f08f86ef957b9342465395f1e75d
Petrus Nguyen Thai Hoc <hoc081098@gmail.com>
2022-11-04 00:07:55 +0700

    @internal

--- lib/src/utils/subscription.dart
+++ lib/src/utils/subscription.dart
@@ -2,9 +2,11 @@ import 'dart:async';
 
 import 'package:rxdart/src/utils/future.dart';
 
+/// @internal
 /// Extensions for [Iterable] of [StreamSubscription]s.
 extension StreamSubscriptionsIterableExtensions
     on Iterable<StreamSubscription<void>> {
+  /// @internal
   /// Pause all subscriptions.
   void pauseAll([Future<void>? resumeSignal]) {
     for (final s in this) {
@@ -12,6 +14,7 @@ extension StreamSubscriptionsIterableExtensions
     }
   }
 
+  /// @internal
   /// Resume all subscriptions.
   void resumeAll() {
     for (final s in this) {
@@ -20,9 +23,11 @@ extension StreamSubscriptionsIterableExtensions
   }
 }
 
+/// @internal
 /// Extensions for [Iterable] of [StreamSubscription]s.
 extension StreamSubscriptionsIterableExtension
     on Iterable<StreamSubscription<void>> {
+  /// @internal
   /// Cancel all subscriptions.
   Future<void>? cancelAll() =>
       waitFuturesList([for (final s in this) s.cancel()]);
