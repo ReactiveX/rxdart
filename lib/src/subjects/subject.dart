f84888914667e43c9b5d0744f32a89a11e6c4a27
Petrus Nguyen Thai Hoc <hoc081098@gmail.com>
2022-11-16 10:54:06 +0700

    docs

--- lib/src/subjects/subject.dart
+++ lib/src/subjects/subject.dart
@@ -19,7 +19,7 @@ abstract class Subject<T> extends StreamView<T> implements StreamController<T> {
   /// This constructor is applicable only for classes that extend [Subject].
   ///
   /// To guarantee the contract of a [Subject], the [controller] must be
-  /// a broadcast [StreamController] and the [stream] must be a broadcast [Stream] as well.
+  /// a broadcast [StreamController] and the [stream] must also be a broadcast [Stream].
   Subject(StreamController<T> controller, Stream<T> stream)
       : _controller = controller,
         assert(stream.isBroadcast, 'Subject requires a broadcast stream'),
