b93683f088152f3e64fcdf0522466c217b735544
suojae <suojae.developer@gmail.com>
2025-03-07 18:52:40 +0900

    refactor(SearchResultWidget): replace Row with ListTile

--- examples/flutter/github_search/lib/widget/search_result_widget.dart
+++ examples/flutter/github_search/lib/widget/search_result_widget.dart
@@ -17,57 +17,38 @@ class SearchResultWidget extends StatelessWidget {
         return InkWell(
           key: ValueKey(item.url),
           onTap: () => showItem(context, item),
-          child: Container(
-            alignment: FractionalOffset.center,
-            margin: const EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 12.0),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Container(
-                  margin: const EdgeInsets.only(right: 16.0),
-                  child: Hero(
-                    tag: item.fullName,
-                    child: ClipOval(
-                      child: Image.network(
-                        item.avatarUrl,
-                        width: 56.0,
-                        height: 56.0,
-                      ),
-                    ),
+          child: Padding(
+            padding:
+                const EdgeInsets.symmetric(horizontal: 16.0, vertical: 12.0),
+            child: ListTile(
+              leading: Hero(
+                tag: item.fullName,
+                child: ClipOval(
+                  child: Image.network(
+                    item.avatarUrl,
+                    width: 56.0,
+                    height: 56.0,
                   ),
                 ),
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Container(
-                        margin: const EdgeInsets.only(
-                          top: 6.0,
-                          bottom: 4.0,
-                        ),
-                        child: Text(
-                          item.fullName,
-                          maxLines: 1,
-                          overflow: TextOverflow.ellipsis,
-                          style: const TextStyle(
-                            fontFamily: 'Montserrat',
-                            fontSize: 16.0,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                      Text(
-                        item.url,
-                        style: const TextStyle(
-                          fontFamily: 'Hind',
-                        ),
-                        maxLines: 1,
-                        overflow: TextOverflow.ellipsis,
-                      )
-                    ],
-                  ),
-                )
-              ],
+              ),
+              title: Text(
+                item.fullName,
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+                style: const TextStyle(
+                  fontFamily: 'Montserrat',
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              subtitle: Text(
+                item.url,
+                style: const TextStyle(
+                  fontFamily: 'Hind',
+                ),
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+              ),
             ),
           ),
         );
